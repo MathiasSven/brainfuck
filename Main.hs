d80d7458b8ced8bb27452332ac55ea7fbd89eda5
Mathias Sven <mathiassven2@hotmail.com>
2024-01-25 22:05:55 +0000

    PrimVar isntead of IORef, and Unboxed.Mutable instead of Mutable Vectors

--- Main.hs
+++ Main.hs
@@ -5,19 +5,20 @@
 
 module Main where
 
-import           Control.Monad            (forM_, join, liftM2, unless)
-import           Data.ByteString.Internal (c2w, w2c)
-import           Data.Function            (fix, (&))
-import           Data.Functor             (($>), (<&>))
-import           Data.IORef               (modifyIORef, newIORef, readIORef)
-import           Data.Text                (Text)
-import qualified Data.Text.IO             as T
-import qualified Data.Vector.Mutable      as MV
-import           Data.Word                (Word16, Word8)
-import           System.Environment       (getArgs)
-import           Text.Parsec              (between, char, choice, noneOf, parse,
-                                           sepEndBy, skipMany, (<|>))
-import           Text.Parsec.Text         (Parser)
+import           Control.Monad               (forM_, join, liftM2, unless)
+import           Data.ByteString.Internal    (c2w, w2c)
+import           Data.Function               (fix, (&))
+import           Data.Functor                (($>), (<&>))
+import           Data.Primitive              (Prim)
+import           Data.Primitive.PrimVar      (modifyPrimVar, newPrimVar,
+                                              readPrimVar)
+import qualified Data.Text.IO                as T
+import qualified Data.Vector.Unboxed.Mutable as MV
+import           Data.Word                   (Word16, Word8)
+import           System.Environment          (getArgs)
+import           Text.Parsec                 (between, char, choice, noneOf,
+                                              parse, sepEndBy, skipMany, (<|>))
+import           Text.Parsec.Text            (Parser)
 
 
 type Brainfuck = [Expr]
@@ -51,24 +52,24 @@ brainfuckParser = ignore *> sepEndBy (Op <$> choice opParsers <|> loopParser) ig
     loopParser = Loop <$> between (char '[') (char ']') brainfuckParser
     ignore = skipMany $ noneOf ((opChar <$> [OpInc ..]) ++ ['[', ']'])
 
-runBrainfuck :: forall a. (Num a, Integral a, Bounded a) => Brainfuck -> IO ()
+runBrainfuck :: forall a. (Integral a, Bounded a, Prim a) => Brainfuck -> IO ()
 runBrainfuck bf = do
-  dataPointer <- newIORef (0 :: a)
+  dataPointer <- newPrimVar (0 :: a)
   arr <- MV.replicate @_ @Word8 (fromIntegral (maxBound @a) + 1) 0
 
-  let readRef = fromIntegral <$> readIORef dataPointer
-      modifyRef = modifyIORef dataPointer
+  let readPV = fromIntegral <$> readPrimVar dataPointer
+      modifyPV = modifyPrimVar dataPointer
 
   bf & fix \evalLoop bf' ->
     forM_ bf' \case
-      Op OpInc    -> readRef >>= MV.modify arr (+ 1)
-      Op OpDec    -> readRef >>= MV.modify arr (- 1)
-      Op OpLeft   -> modifyRef (- 1)
-      Op OpRight  -> modifyRef (+ 1)
-      Op OpOutput -> readRef >>= MV.read arr >>= putChar . w2c
-      Op OpInput  -> join $ liftM2 (MV.write arr) readRef (c2w <$> getChar)
+      Op OpInc    -> readPV >>= MV.modify arr (+ 1)
+      Op OpDec    -> readPV >>= MV.modify arr (- 1)
+      Op OpLeft   -> modifyPV (- 1)
+      Op OpRight  -> modifyPV (+ 1)
+      Op OpOutput -> readPV >>= MV.read arr >>= putChar . w2c
+      Op OpInput  -> join $ liftM2 (MV.write arr) readPV (c2w <$> getChar)
       Loop bf''   -> fix \recurse -> do
-        state <- readRef >>= MV.read arr
+        state <- readPV >>= MV.read arr
         unless (state == 0) (evalLoop bf'' >> recurse)
 
   putChar '\n'
